2317c34a6b1b934cbd387832da3398a671738a97
anikatasnin <jarintasnin09@gmail.com>
2025-05-21 18:57:27 +0600

    create form

--- src/components/AddTea.jsx
+++ src/components/AddTea.jsx
@@ -2,8 +2,49 @@ import React from "react";
 
 const AddTea = () => {
   return (
-    <div>
-      <h2>Add Tea</h2>
+    <div className="bg-[#F4F3F0] p-24">
+      <h2 className="text-3xl font-extrabold">Add Tea</h2>
+      <form action="">
+        <div className="flex flex-col gap-10">
+          <div className="flex gap-10">
+            <div className="flex flex-col">
+              <label htmlFor="">Tea Name</label>
+              <input
+                type="text"
+                placeholder="Enter tea name"
+                className="input w-96"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Available quantity</label>
+              <input
+                type="text"
+                placeholder="Enter tea name"
+                className="input w-96"
+              />
+            </div>
+          </div>
+          <div className="flex gap-10">
+            <div className="flex flex-col">
+              <label htmlFor="">Supplier</label>
+              <input
+                type="text"
+                placeholder="Enter tea name"
+                className="input w-96"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Details</label>
+              <input
+                type="text"
+                placeholder="Enter tea name"
+                className="input w-96"
+              />
+            </div>
+          </div>
+          <button className="btn">Add Tea</button>
+        </div>
+      </form>
     </div>
   );
 };
