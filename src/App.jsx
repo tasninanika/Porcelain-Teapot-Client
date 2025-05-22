4fead781bae59ad34da13ffdf569d0d4310a8fce
anikatasnin <jarintasnin09@gmail.com>
2025-05-22 22:10:15 +0600

    apply style on div

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl text-purple-600">Porcelain Tea</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 md:px-10 my-10">
-        {teas.map((tea) => (
-          <TeaCard key={tea._id} tea={tea}></TeaCard>
-        ))}
+      <div className="w-11/12 mx-auto">
+        <h1 className="text-6xl text-purple-600 text-center">Porcelain Tea</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 md:px-10 my-10">
+          {teas.map((tea) => (
+            <TeaCard key={tea._id} tea={tea}></TeaCard>
+          ))}
+        </div>
       </div>
     </>
   );
