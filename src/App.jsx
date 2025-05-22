90db56c1aa18e17b1229afc13db0f980121e5478
anikatasnin <jarintasnin09@gmail.com>
2025-05-22 21:35:32 +0600

    map tea data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import "./App.css";
+import TeaCard from "./components/TeaCard";
 
 function App() {
   const teas = useLoaderData();
@@ -7,6 +8,9 @@ function App() {
   return (
     <>
       <h1 className="text-6xl text-purple-600">Porcelain Tea</h1>
+      {teas.map((tea) => (
+        <TeaCard key={tea._id} tea={tea}></TeaCard>
+      ))}
     </>
   );
 }
