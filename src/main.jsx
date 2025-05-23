8a660779d29131c7aa2a5644be1f740ab6eba7dc
anikatasnin <jarintasnin09@gmail.com>
2025-05-24 00:16:38 +0600

    create auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import UpdateTea from "./components/UpdateTea";
 import App from "./App";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import AuthProvider from "./providers/AuthProvider";
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
