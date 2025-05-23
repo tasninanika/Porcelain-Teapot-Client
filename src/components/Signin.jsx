6730cd4e539c7b99ab85511cbb7d87e00365db14
anikatasnin <jarintasnin09@gmail.com>
2025-05-24 00:01:10 +0600

    setup firebase

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../Header";
 
 const Signin = () => {
   return (
     <div>
+      <Header></Header>
       <h2>Sign in</h2>
     </div>
   );
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9YkZmAF1efnvMpxbuASjlHnu7PKocMjA",
+  authDomain: "porcelain-tea.firebaseapp.com",
+  projectId: "porcelain-tea",
+  storageBucket: "porcelain-tea.firebasestorage.app",
+  messagingSenderId: "816616943957",
+  appId: "1:816616943957:web:590e33671d3b7e6b437fdc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
