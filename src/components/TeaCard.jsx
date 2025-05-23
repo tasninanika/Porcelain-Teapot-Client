dff3af3b22ea25e1f58bdaffab3e5892d5483b3d
anikatasnin <jarintasnin09@gmail.com>
2025-05-23 09:15:20 +0600

    add onclick on delete btn

--- src/components/TeaCard.jsx
+++ src/components/TeaCard.jsx
@@ -1,10 +1,28 @@
 import { FaEye, FaPen, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const TeaCard = ({ tea }) => {
   const { _id, name, chef, url } = tea;
 
   const handleDelete = (_id) => {
     console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
   };
 
   return (
