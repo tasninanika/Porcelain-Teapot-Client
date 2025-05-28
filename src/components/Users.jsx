3e32f248d2cea3e4e117f56f5f4157297085e7d4
anikatasnin <jarintasnin09@gmail.com>
2025-05-28 12:18:05 +0600

    show users data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -7,6 +7,42 @@ const Users = () => {
   return (
     <div>
       <h2 className="text-3xl">Users: {users.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Created At</th>
+              <th>Last Login At</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {users.map((user) => (
+              <tr key={user._id}>
+                <th>1</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.lastSignInTime}</td>
+                <td>
+                  <button className="btn">E</button>
+                  <button
+                    onClick={() => handleUserDelete(user._id)}
+                    className="btn"
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
