be277e633117960bb0f2eae0fcf3b8e089184339
anikatasnin <jarintasnin09@gmail.com>
2025-05-28 14:48:52 +0600

    add delete users data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,41 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
   const [users, setUsers] = useState(loadedUsers);
+  const handleUserDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // delete from the database
+        fetch(`https://coffee-store-server-delta-sage.vercel.app/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+
+              const remainingUsers = users.filter((user) => user._id !== id);
+              setUsers(remainingUsers);
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
       <h2 className="text-3xl">Users: {users.length}</h2>
