f40bec424f019ec9fda1f3c09c7e4684be93c298
anikatasnin <jarintasnin09@gmail.com>
2025-05-28 12:06:35 +0600

    load users data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState();
   return (
     <div>
       <h2 className="text-3xl">Users</h2>
